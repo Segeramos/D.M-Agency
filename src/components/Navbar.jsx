d07f27f24c0138aa08f46dd3686b5908549acfb8
Segera <your.segeramos89@gmail.com>
2025-06-10 11:06:13 +0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
 
+
+const navItems = [
+    {
+        path: "/",
+        name  : 'Home'
+      },
+       {
+        path: "/about",
+        element: <About />
+      },
+       {
+        path: "/services",
+        element: <Services />
+      },
+       {
+        path: "/ourWork",
+        element: <OurWork />
+      },
+       {
+        path: "/contact",
+        element: <Contact />
+      },
+]
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>Navbar</div> 
   )
 }
 
